1866b16b0c159f21f7fda4dffb985ed0ea19906b
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-31 11:05:08 +0545

    added header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Home />
     </div>
   )
