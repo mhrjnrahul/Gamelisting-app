945e948970f7f6e93e809d7fd8f6249639988ca6
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-31 12:08:43 +0545

    added responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='grid grid-cols-4'>
+        <div className='h-full bg-red-600 hidden md:block'>
+            Genre
+        </div>
+        <div className='col-span-4 md:col-span-3'>
+            Game List
+        </div>
+    </div>
   )
 }
 
