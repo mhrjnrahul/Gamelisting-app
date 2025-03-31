892807dcc7352de929cc39fdf4758a4acc9b8ab1
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-31 11:36:58 +0545

    theme setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from './pages/Home'
 import Header from './components/Header'
 
 const App = () => {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div>
+    <div className={`${theme === 'light' ? 'bg-white' : 'bg-black'} min-h-screen`}>
       <Header />
       <Home />
     </div>
